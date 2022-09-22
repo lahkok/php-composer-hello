d018244dfdde687baacbec8ec04f6de77df6f147
lahkok <masaidahmadirfan@gmail.com>
2022-09-23 04:15:42 +0700

    Contoh Library Sederhana

---
+++ src/Customer.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Nyukk\Belajar;
+
+class Customer
+{
+    public function __construct(private string $name)
+    {
+    }
+
+    public function sayHello(string $name): string
+    {
+        return "Hello $name, my name is $this->name";
+    }
+}
